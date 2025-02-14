996c00271232bf9e70d47ba596e9e9fa93c58e22
madhavdandona123 <71923095+madhavdandona123@users.noreply.github.com>
2025-02-14 20:52:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('model/traffic_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    prediction = model.predict([data])
+    return render_template('index.html', prediction_text=f'Traffic Prediction: {prediction[0]}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
